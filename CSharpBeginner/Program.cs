86ebee1bdfcb83a85329895ad2da7864865aff07
AmirCodeLip <55617930+AmirCodeLip@users.noreply.github.com>
2024-01-10 07:25:18 -0800

    up v6

--- CSharpBeginner/Program.cs
+++ CSharpBeginner/Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("point");
+﻿//string name = null;
+
+
+//for (int i = 0; i < 3; i++)
+//{
+//    name = Console.ReadLine();
+//    Console.WriteLine(name);
+//}
+
+//for (int i = 3; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int min = 100;
+
+//for (int j = 0; j < 10; j++)
+//{
+//    int number = int.Parse(Console.ReadLine());
+//    if (number < min)
+//    {
+//        min = number;
+//    }
+//}
+
+//Console.WriteLine(min);
+int max = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < max; i += 2)
+{
+    //if(i % 2  == 0)
+    //{
+    //    Console.WriteLine(i);
+    //}
+    Console.WriteLine(i);
+
+}
+
--- CSharpBeginner/obj/Debug/net7.0/CSharpBeginner.AssemblyInfo.cs
+++ CSharpBeginner/obj/Debug/net7.0/CSharpBeginner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpBeginner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e29a9902c6a2206be196f2e03a41a818681bcc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4dc246e9c4553b64ece99b5866d86b38c0ea92")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpBeginner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpBeginner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
